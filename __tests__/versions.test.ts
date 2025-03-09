30e03e87eb93eb8dac2cbeb2276f71f7eff91cc7
Sylvain Dumas <dumassylvain59@gmail.com>
2025-03-09 11:43:27 +0000

    feat: enhance CLI version retrieval with support for version ranges and add manifest generation script

--- __tests__/versions.test.ts
+++ __tests__/versions.test.ts
@@ -1,20 +1,45 @@
 import * as versions from '../src/versions'
+import * as manifest from '../src/versions.manifest.js'
 
 describe('getCliVersion', () => {
-  it('should return the requested version if it is in a supported format', () => {
-    const version = '1.2.3.4'
+  it('should return the latest version if the input is "latest"', () => {
+    const version = 'latest'
     const result = versions.getCliVersion(version)
-    expect(result).toBe(version)
+    expect(result).toBe(manifest.CLI_VERSION_LATEST)
   })
 
-  const invalidVersions = ['1.2.3', '1.2', 'invalid-version']
-  invalidVersions.forEach((version) => {
-    it(`should throw an error for unsupported version format: ${version}`, () => {
-      expect(() => versions.getCliVersion(version)).toThrow(
-        `Unsupported version format: ${version}`
-      )
+  const wellKnownVersions = ['7.0.2.4839', '6.2.0.4584', '5.0.0.2966']
+  wellKnownVersions.forEach((version) => {
+    it(`should return the requested version if it is a well-known version: ${version}`, () => {
+      const result = versions.getCliVersion(version)
+      expect(result).toBe(version)
     })
   })
+
+  const expectedVersionByRange = {
+    '>=5.0.0': manifest.CLI_VERSION_LATEST,
+    '<6.0.0': '5.0.1.3006',
+    '>=6.0.0 <7.0.0': '6.2.1.4610',
+    '3.x': '3.4.0.1729',
+    '3.0.x': '3.0.3.778',
+    '3.0.0': '3.0.0.702',
+    '5.1 || >=6.1 <7.0.0': '6.2.1.4610'
+  }
+  for (const [range, cliVersion] of Object.entries(expectedVersionByRange)) {
+    it(`should return the expected version for the range: ${range} to be ${cliVersion}`, () => {
+      const result = versions.getCliVersion(range)
+      expect(result).toBe(cliVersion)
+    })
+  }
+
+  const invalidVersions = ['invalid-version']
+  for (const version of invalidVersions) {
+    it(`should throw an error for unsupported version format: ${version}`, () => {
+      expect(() => versions.getCliVersion(version)).toThrow(
+        `No CLI version found for: ${version}`
+      )
+    })
+  }
 })
 
 describe('getGhCacheVersion', () => {
@@ -23,15 +48,6 @@ describe('getGhCacheVersion', () => {
     const result = versions.getGhCacheVersion(version)
     expect(result).toBe('1.2.3')
   })
-
-  const invalidVersions = ['1.2.3', '1.2', 'invalid-version']
-  invalidVersions.forEach((version) => {
-    it(`should throw an error for unsupported version format: ${version}`, () => {
-      expect(() => versions.getGhCacheVersion(version)).toThrow(
-        `Unsupported version format: ${version}`
-      )
-    })
-  })
 })
 
 describe('getVersionSource', () => {
@@ -39,13 +55,6 @@ describe('getVersionSource', () => {
 
   afterEach(() => {
     Object.defineProperty(process, 'platform', { value: originalPlatform })
-  })
-
-  it('should throw an error for unsupported version format', () => {
-    const version = 'invalid-version'
-    expect(() => versions.getVersionSource(version)).toThrow(
-      `Unsupported version format: ${version}`
-    )
   })
 
   const platforms = {
