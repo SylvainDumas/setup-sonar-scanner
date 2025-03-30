e189285c4dc8888a53d46c0d815abbaf96ea8615
Sylvain Dumas <dumassylvain59@gmail.com>
2025-03-30 20:34:12 +0000

    refactor(tests): simplify downloadTool tests and improve mock handling

--- __tests__/install.test.ts
+++ __tests__/install.test.ts
@@ -12,7 +12,7 @@ process.env['RUNNER_TEMP'] = tempPath
 const testdataPath = path.join(process.cwd(), '__tests__', 'testdata')
 
 const IS_WINDOWS = process.platform === 'win32'
-const IS_MAC = process.platform === 'darwin'
+//const IS_MAC = process.platform === 'darwin'
 
 function mkTempDir(): string {
   return path.join(os.tmpdir(), `${crypto.randomUUID()}`)
@@ -30,28 +30,11 @@ describe('downloadTool', () => {
     nock.cleanAll()
   })
 
-  const mockDonwload = async function (
-    file: string,
-    extension: string
-  ): Promise<string> {
-    const testUrl = 'http://example.com'
-    const filename = `${file}.${extension}`
-    const mockSource: install.Source = {
-      url: `${testUrl}/${filename}`,
-      digest: 'some-digest'
-    }
-    nock(testUrl)
-      .get(`/${filename}`)
-      .replyWithFile(200, path.join(testdataPath, filename))
-
-    return install.downloadTool(mockSource)
-  }
-
   for (const extension of ['zip', '7z', 'tar.gz']) {
-    if (extension === '7z' && IS_WINDOWS) continue
+    if (extension === '7z' && !IS_WINDOWS) continue
 
     it(`${extension}: should download & extract the tool and change to directory bin`, async () => {
-      const result = await mockDonwload('bin', extension)
+      const result = await install.downloadTool(mockSource('bin', extension))
 
       expect(result.startsWith(tempPath)).toBe(true)
       expect(result.endsWith('bin')).toBe(true)
@@ -61,38 +44,23 @@ describe('downloadTool', () => {
   }
 
   it('should throw an error for unsupported file extension', async () => {
-    const unsupportedSource: install.Source = {
-      url: 'http://example.com/tool.unsupported',
-      digest: 'some-digest'
-    }
-    nock('http://example.com')
-      .get('/tool.unsupported')
-      .replyWithFile(200, path.join(testdataPath, 'tool.unsupported'))
-
+    const unsupportedSource: install.Source = mockSource('tool', 'unsupported')
     await expect(install.downloadTool(unsupportedSource)).rejects.toThrow(
       'Unable to find extract function for file: tool.unsupported'
     )
   })
-
-  /*
-  it('should return the extract path if package does not contain a single directory', async () => {
-    const mockPackagePath = '/path/to/downloaded/tool.zip'
-    const mockExtractPath = '/path/to/extracted/tool'
-
-    ;(downloadTool as jest.Mock<typeof downloadTool>).mockResolvedValue(
-      mockPackagePath
-    )
-    ;(extractZip as jest.Mock<typeof extractZip>).mockResolvedValue(
-      mockExtractPath
-    )
-    ;(fs.readdirSync as jest.Mock).mockReturnValue(['file1', 'file2'])
-
-    const result = await install.downloadTool(mockSource)
-
-    expect(downloadTool).toHaveBeenCalledWith(mockSource.url)
-    expect(extractZip).toHaveBeenCalledWith(mockPackagePath)
-    expect(fs.readdirSync).toHaveBeenCalledWith(mockExtractPath)
-    expect(result).toBe(mockExtractPath)
-  })
-    */
 })
+
+function mockSource(file: string, extension: string): install.Source {
+  const testUrl = 'http://example.com'
+  const filename = `${file}.${extension}`
+  const mockSource: install.Source = {
+    url: `${testUrl}/${filename}`,
+    digest: 'some-digest'
+  }
+  nock(testUrl)
+    .get(`/${filename}`)
+    .replyWithFile(200, path.join(testdataPath, filename))
+
+  return mockSource
+}
